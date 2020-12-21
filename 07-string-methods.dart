95935ef6883c78753f5455f7960105dd86d8cbd7
sarthaksavvy <bitfumes@gmail.com>
2020-12-21 12:58:39 +0530

    07 learn dart for beginner - strings methods

---
+++ 07-string-methods.dart
@@ -0,0 +1,10 @@
+main() {
+  var name = 'Sarthak Shrivastava';
+  // print(name.isNotEmpty);
+  // print(name.codeUnits);
+  // print(name.toUpperCase());
+  // print(name.toLowerCase());
+  // print(name.length);
+  // print(name.trim().length);
+  // print(name.replaceAll('Sarthak', 'John'));
+}
