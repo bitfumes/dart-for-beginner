1f0c544f374d01c1a45bb7a40b668dc58f1a657b
sarthaksavvy <bitfumes@gmail.com>
2020-12-21 12:21:41 +0530

    04 learn dart for beginner - numbers

---
+++ 04-numbers.dart
@@ -0,0 +1,12 @@
+main() {
+  var pi = 'the value of pi is 3.1419';
+  print(pi.runtimeType);
+  var newPI = double.parse(pi);
+  print(newPI.runtimeType);
+  // number 64 bit
+  // int bitcoinPrice = 240000;
+  // double bitcoinExactPrice = 24000.2342;
+
+  // print('Bitcoin price is :' + bitcoinPrice.toString());
+  // print('Bitcoin exact price is :' + bitcoinExactPrice.toStringAsFixed(2));
+}
