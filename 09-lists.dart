33c957238a7fbe61c27335ad40eacf0e298339d9
sarthaksavvy <bitfumes@gmail.com>
2020-12-22 19:35:06 +0530

    09 learn dart for beginner - lists

---
+++ 09-lists.dart
@@ -0,0 +1,9 @@
+main() {
+  var names1 = ['Sarthak', 'John', 'Jenny'];
+  var names2 = ['Sam', 'Joe'];
+
+  var allNames = [...names1, ...names2];
+  print(allNames);
+  // print(names);
+  // print(names[5]);
+}
