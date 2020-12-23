d89c1f03bf013e7b0a7ef07f55d41b3366b1f6d2
sarthaksavvy <bitfumes@gmail.com>
2020-12-23 13:06:29 +0530

    13 learn dart for beginner - arithmetics

---
+++ 13-arithmetics.dart
@@ -0,0 +1,13 @@
+main() {
+  var a = 5;
+  var b = 2;
+  var c = a--;
+  print(c);
+  print(a);
+  // print(a + b);
+  // print(a - b);
+  // print(a * b);
+  // print(a / b);
+  // print(a ~/ b);
+  // print(a % b);
+}
