1a8b2f6f39bcd212016746461baf2ba9fdf26a15
sarthaksavvy <bitfumes@gmail.com>
2020-12-24 19:45:39 +0530

    15-logical-

---
+++ 15-logical-&-bitwise-operation.dart
@@ -0,0 +1,14 @@
+main() {
+  // OR is ||
+  // AND is &&
+  // & AND
+  // | OR
+  // ^ XOR
+  var x = 2; // 10
+  var y = 3; // 11
+  // ........// 01
+  //  001
+  print(3 >> 1);
+  // print(2 & 3);
+  // print(x == y && x < y && x > 40);
+}
