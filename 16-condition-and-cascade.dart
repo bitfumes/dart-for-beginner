869fd04b865ba8ce566e90cab25870878a400ea1
sarthaksavvy <bitfumes@gmail.com>
2020-12-30 18:35:52 +0530

    17 learn dart for beginner - functions

---
+++ 16-condition-and-cascade.dart
@@ -0,0 +1,8 @@
+main() {
+  // var a = 10;
+  // var b = 5;
+  var user = [];
+  user..add('Sarthak')..add('John')..add('Al');
+
+  print(user);
+}
---
+++ 17-functions.dart
@@ -0,0 +1,5 @@
+void main() {
+  int sum(a, b) => a + b;
+
+  print(sum(2, 43));
+}
