3a2cbc0094c1fca26484871bcf0c69ef8d3611a4
sarthaksavvy <bitfumes@gmail.com>
2020-12-31 19:45:51 +0530

    23 - learn dart for beginner - Loops

---
+++ 22-loops.dart
@@ -0,0 +1,21 @@
+main() {
+  // var myName = 'Sarthak';
+  // for (var letter in myName.split('')) {
+  //   print(letter);
+  // }
+  var names = ['Sarthak', 'John', 'Jeny', 'Jackson', 'Joe', 'Jolly'];
+  var searchTerm = 'jO';
+  // names.forEach((name) {
+  //   if (name.toLowerCase().contains(searchTerm.toLowerCase())) {
+  //     print(name);
+  //   }
+  // });
+  // for (var name in names) {
+  //   if (name.toLowerCase().contains(searchTerm.toLowerCase())) {
+  //     print(name);
+  //   }
+  // }
+  // for (var i = 0; i < names.length; i++) {
+  //   print(names[i]);
+  // }
+}
